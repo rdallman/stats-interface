f6677e6dfad5e6edbfb2da9dd53242365a8b1d9b
Reed Allman <reed@rdallman.com>
2020-10-30 10:52:07 -0700

    pushes down future builder for pair page (#16)
    still dk what I'm doing but this is lower and seems to work

--- lib/pages/pair_page.dart
+++ lib/pages/pair_page.dart
@@ -39,228 +39,227 @@ class _PairPageState extends State<PairPage> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO idk what I'm doing but we should conditionally return a future builder only
-    // if some fields we need aren't filled in, otherwise we can just return the thing.
-    return FutureBuilder<Pair>(
-      future: pairF,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Styles.errorText(snapshot.error.toString());
-        }
-        if (snapshot.hasData) {
-          // TODO this is a mess
-          var data = snapshot.data;
-          Pair pair = data;
-          var token0Address = data.token0;
-          var token1Address = data.token1;
-
-          var token0Symbol = widget.pair.split("-")[0];
-          var token1Symbol = widget.pair.split("-")[1];
-
-          String fees = (double.parse(
-                      widget.volume.replaceAll(RegExp(r'[^\w\s .-]+'), '')) *
-                  0.003)
-              .toStringAsFixed(2);
-          var screenSize = MediaQuery.of(context).size;
-          double appBarHeight = AppBar().preferredSize.height;
-          // Could refactor this code
-          return Scaffold(
-            backgroundColor: Colors.white,
-            appBar: AppBar(
-              title: Row(
-                children: [
-                  SizedBox(
-                      height: 30, child: Image.asset('assets/logo-white.png')),
-                  //  Text("Stats")
-                ],
-              ),
-              actions: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: RaisedButton(
-                    color: Colors.white,
-                    child: Text("Go to GoSwap",
-                        style: TextStyle(color: Colors.blue)),
-                    onPressed: () {
-                      launch('https://goswap.exchange');
-                    },
-                  ),
-                ),
-              ],
+    String fees = (double.parse(
+                widget.volume.replaceAll(RegExp(r'[^\w\s .-]+'), '')) *
+            0.003)
+        .toStringAsFixed(2);
+    var screenSize = MediaQuery.of(context).size;
+    double appBarHeight = AppBar().preferredSize.height;
+    // Could refactor this code
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Row(
+          children: [
+            SizedBox(
+                height: 30, child: Image.asset('assets/logo-white.png')),
+            //  Text("Stats")
+          ],
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: RaisedButton(
+              color: Colors.white,
+              child: Text("Go to GoSwap",
+                  style: TextStyle(color: Colors.blue)),
+              onPressed: () {
+                launch('https://goswap.exchange');
+              },
             ),
-            body: SingleChildScrollView(
-              child: Container(
-                constraints:
-                    BoxConstraints(minHeight: screenSize.height - appBarHeight),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: const [
-                      Color(0xff2c274c),
-                      Color(0xff46426c),
-                    ],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                  ),
-                ),
-                child: Center(
-                  child: Container(
-                    constraints: BoxConstraints(maxWidth: 1200),
-                    padding: EdgeInsets.all(15),
-                    child: Column(
-                      children: [
-                        Text(
-                          '${widget.pair} Pair',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 2),
-                          textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          constraints:
+              BoxConstraints(minHeight: screenSize.height - appBarHeight),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: const [
+                Color(0xff2c274c),
+                Color(0xff46426c),
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+          ),
+          child: FutureBuilder<Pair>(
+            future: pairF,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Styles.errorText(snapshot.error.toString());
+              }
+              if (!snapshot.hasData) {
+                return Styles.waiting();
+              }
+
+              // TODO this is a mess
+              var data = snapshot.data;
+              Pair pair = data;
+              var token0Address = data.token0;
+              var token1Address = data.token1;
+
+              var token0Symbol = widget.pair.split("-")[0];
+              var token1Symbol = widget.pair.split("-")[1];
+
+              return Center(
+                child: Container(
+                  constraints: BoxConstraints(maxWidth: 1200),
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    children: [
+                      Text(
+                        '${widget.pair} Pair',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      Wrap(
+                        children: [
+                          InfoContainer(
+                            value: pair.toString(),
+                            title: 'Pair Name',
+                            copy: false,
+                            token1Address: pair.token1,
+                            tokenAddress: pair.token0,
+                          ),
+                          ReusableContainer(
+                            title: 'Liquidity',
+                            value: widget.liquidity,
+                          ),
+                          ReusableContainer(
+                            title: 'Volume (24hr)',
+                            value: widget.volume,
+                          ),
+                          ReusableContainer(
+                            title: 'Fees (24hr)',
+                            value:
+                                Globals.formatCurrency(Globals.toDec(fees)),
+                          ),
+                        ],
+                      ),
+                      Card(
+                        child: Container(
+                          constraints:
+                              BoxConstraints(minWidth: 100, maxWidth: 800),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 10),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  FlatButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isShowingVolume = true;
+                                      });
+                                    },
+                                    child: Text(
+                                      'Volume',
+                                      style: TextStyle(
+                                        color: isShowingVolume
+                                            ? activeButtonColor
+                                            : inactiveButtonColor,
+                                        fontSize: 16,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 1.0,
+                                    height: 20.0,
+                                    color: Colors.grey,
+                                  ),
+                                  FlatButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isShowingVolume = false;
+                                      });
+                                    },
+                                    child: Text(
+                                      'Liquidity',
+                                      style: TextStyle(
+                                        color: !isShowingVolume
+                                            ? activeButtonColor
+                                            : inactiveButtonColor,
+                                        fontSize: 16,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              isShowingVolume
+                                  ? PairVolumeChart(
+                                      pairAddress: widget.pairAddress,
+                                    )
+                                  : PairLiquidityChart(
+                                      pairAddress: widget.pairAddress,
+                                    ),
+                            ],
+                          ),
                         ),
-                        const SizedBox(
-                          height: 10.0,
-                        ),
-                        Wrap(
-                          children: [
-                            InfoContainer(
-                              value: pair.toString(),
-                              title: 'Pair Name',
-                              copy: false,
-                              token1Address: pair.token1,
-                              tokenAddress: pair.token0,
-                            ),
-                            ReusableContainer(
-                              title: 'Liquidity',
-                              value: widget.liquidity,
-                            ),
-                            ReusableContainer(
-                              title: 'Volume (24hr)',
-                              value: widget.volume,
-                            ),
-                            ReusableContainer(
-                              title: 'Fees (24hr)',
-                              value:
-                                  Globals.formatCurrency(Globals.toDec(fees)),
-                            ),
-                          ],
-                        ),
-                        Card(
-                          child: Container(
-                            constraints:
-                                BoxConstraints(minWidth: 100, maxWidth: 800),
-                            padding: EdgeInsets.symmetric(
-                                vertical: 16, horizontal: 10),
-                            child: Column(
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        '${widget.pair} Information',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2),
+                        textAlign: TextAlign.center,
+                      ),
+                      Card(
+                        child: Container(
+                          constraints:
+                              BoxConstraints(minWidth: 100, maxWidth: 800),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 10),
+                          child: Center(
+                            child: Wrap(
+                              runSpacing: 10.0,
                               children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    FlatButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          isShowingVolume = true;
-                                        });
-                                      },
-                                      child: Text(
-                                        'Volume',
-                                        style: TextStyle(
-                                          color: isShowingVolume
-                                              ? activeButtonColor
-                                              : inactiveButtonColor,
-                                          fontSize: 16,
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                    Container(
-                                      width: 1.0,
-                                      height: 20.0,
-                                      color: Colors.grey,
-                                    ),
-                                    FlatButton(
-                                      onPressed: () {
-                                        setState(() {
-                                          isShowingVolume = false;
-                                        });
-                                      },
-                                      child: Text(
-                                        'Liquidity',
-                                        style: TextStyle(
-                                          color: !isShowingVolume
-                                              ? activeButtonColor
-                                              : inactiveButtonColor,
-                                          fontSize: 16,
-                                        ),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                  ],
+                                InfoContainer(
+                                  value: widget.pair,
+                                  title: 'Pair Name',
+                                  copy: false,
                                 ),
-                                isShowingVolume
-                                    ? PairVolumeChart(
-                                        pairAddress: widget.pairAddress,
-                                      )
-                                    : PairLiquidityChart(
-                                        pairAddress: widget.pairAddress,
-                                      ),
+                                InfoContainer(
+                                  value: widget.pairAddress,
+                                  title: 'Pair Address',
+                                ),
+                                InfoContainer(
+                                  value: token0Address,
+                                  title: '${token0Symbol} Address',
+                                ),
+                                InfoContainer(
+                                  value: token1Address,
+                                  title: '${token1Symbol} Address',
+                                ),
                               ],
                             ),
                           ),
                         ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          '${widget.pair} Information',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 2),
-                          textAlign: TextAlign.center,
-                        ),
-                        Card(
-                          child: Container(
-                            constraints:
-                                BoxConstraints(minWidth: 100, maxWidth: 800),
-                            padding: EdgeInsets.symmetric(
-                                vertical: 16, horizontal: 10),
-                            child: Center(
-                              child: Wrap(
-                                runSpacing: 10.0,
-                                children: [
-                                  InfoContainer(
-                                    value: widget.pair,
-                                    title: 'Pair Name',
-                                    copy: false,
-                                  ),
-                                  InfoContainer(
-                                    value: widget.pairAddress,
-                                    title: 'Pair Address',
-                                  ),
-                                  InfoContainer(
-                                    value: token0Address,
-                                    title: '${token0Symbol} Address',
-                                  ),
-                                  InfoContainer(
-                                    value: token1Address,
-                                    title: '${token1Symbol} Address',
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-              ),
-            ),
-          );
-        }
-        return Text('Loading...');
-      },
+              );
+            }
+          ),
+        ),
+      ),
     );
   }
 }
